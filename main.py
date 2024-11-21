16cf1d190301d2998352bd16fad3fd50edc43f50
amirnak <amirnakgamer@gmail.com>
2024-11-21 23:23:23 +0300

    Fix: PEP8 in main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from src.parse_html import parse_html
 from src.handle_html import create_projects
 from src.google_api import write_to_table
 
+
 def main() -> None:
     id_table = ID_TABLE
     name_worksheet = NAME_SPREADSHEET
@@ -22,7 +23,7 @@ def main() -> None:
     creds = ServiceAccountCredentials.from_json_keyfile_name(
         "credentials.json", scope)
     client = gspread.authorize(creds)
-    
+
     html = parse_html()
     handled_html = create_projects(html)
     write_to_table(client, id_table, name_worksheet, handled_html)
