ae34ccab159a0c40c1828db0ab42c010c0be0eb8
amirnak <amirnakgamer@gmail.com>
2024-12-21 19:27:55 +0300

    Fix sync and asyns work with functions

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import asyncio
 
+import httpx
 from httpx import AsyncClient
 
-from config import ROSMOL_FORUMS_PATH, ROSMOL_FORUMS_URL
+from config import ROSMOL_FORUMS_PATH, ROSMOL_FORUMS_URL, ROSMOL_FORUMS_PARAM
 from utils import get_forums, handle_forum, request_html
 
 
 async def main():
-    async with AsyncClient() as client:
-        html = await request_html(client, ROSMOL_FORUMS_URL + "/" + ROSMOL_FORUMS_PATH)
+    request = httpx.get(ROSMOL_FORUMS_URL + "/" + ROSMOL_FORUMS_PATH, params=ROSMOL_FORUMS_PARAM)
+    if request.status_code != 200:
+        request.raise_for_status()
 
-    forums = await asyncio.to_thread(get_forums, html)
+    html = request.text
+
+    forums = get_forums(html)
 
     async with AsyncClient() as client:
         tasks = [asyncio.create_task(request_html(client, ROSMOL_FORUMS_URL + forum)) for forum in forums]
         forums_html = await asyncio.gather(*tasks)
 
-    tasks = [asyncio.create_task(asyncio.to_thread(handle_forum, html)) for html in forums_html]
-    result = await asyncio.gather(*tasks)
-    print(result)
+    results = [handle_forum(html) for html in forums_html]
+    print(results)
 
 
 if __name__ == "__main__":
