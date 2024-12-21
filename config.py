5804fa2912d65fb9e87ab7d4e9134d55b75e1def
amirnak <amirnakgamer@gmail.com>
2024-12-21 23:19:29 +0300

    Add logging with loguru

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-import os
 import json
+import os
+import sys
 
 from dotenv import load_dotenv
 
@@ -11,3 +12,9 @@ ROSMOL_FORUMS_URL = os.environ.get("ROSMOL_FORUMS_URL")
 ROSMOL_FORUMS_PATH = os.environ.get("ROSMOL_FORUMS_PATH")
 ROSMOL_FORUMS_PARAM = os.environ.get("ROSMOL_FORUMS_PARAM")
 ROSMOL_FORUMS_PARAM = json.loads(ROSMOL_FORUMS_PARAM)
+with open("config/logs.json", "r", encoding="utf-8") as file:
+    LOGS_CONFIG = json.load(file)
+
+for handler in LOGS_CONFIG["handlers"]:
+    if handler["sink"] == "sys.stdout":
+        handler["sink"] = sys.stdout
--- main.py
+++ main.py
@@ -2,29 +2,40 @@ import asyncio
 
 import httpx
 from httpx import AsyncClient
+from loguru import logger
 
-from config import ROSMOL_FORUMS_PATH, ROSMOL_FORUMS_URL, ROSMOL_FORUMS_PARAM, SPREADSHEET_ID, SHEET_NAME
+from config import ROSMOL_FORUMS_PATH, ROSMOL_FORUMS_URL, ROSMOL_FORUMS_PARAM, SPREADSHEET_ID, SHEET_NAME, LOGS_CONFIG
 from utils import get_forums, handle_forum, request_html, authorize_google_sheets, write_to_google_sheet
 
 
+logger.remove()
+logger.configure(**LOGS_CONFIG)
+
+
+@logger.catch
 async def main():
+    logger.info("Start parsing")
     request = httpx.get(ROSMOL_FORUMS_URL + "/" + ROSMOL_FORUMS_PATH, params=ROSMOL_FORUMS_PARAM)
     if request.status_code != 200:
         request.raise_for_status()
-
     html = request.text
+    logger.debug("Get forums list html")
 
     forums = get_forums(html)
+    logger.debug(f"Found {len(forums)} forums")
 
     async with AsyncClient() as client:
         tasks = [asyncio.create_task(request_html(client, ROSMOL_FORUMS_URL + forum)) for forum in forums]
         forums_html = await asyncio.gather(*tasks)
+    logger.debug("Get forums html")
 
     results = [handle_forum(html) for html in forums_html]
-    print(results)
+    logger.debug("Handle forums html")
 
     service = authorize_google_sheets()
     write_to_google_sheet(service, SPREADSHEET_ID, SHEET_NAME, results)
+    logger.debug("Write to google sheet")
+    logger.info("End parsing")
 
 
 if __name__ == "__main__":
